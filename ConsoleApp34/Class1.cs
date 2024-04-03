18a44839ad25d642a0fc89f62f4da606fd5981f1
Bard1en <greger.grym@gmail.com>
2024-04-03 10:38:04 +0200

    Add project files.

---
+++ ConsoleApp34/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp34
+{
+    class bil
+    {
+        public string  märke;
+        public string modell;
+        public int årsmodell;
+        public string färg;
+        public int mil;
+        public void metod1()
+        {
+            Console.WriteLine("Skriv in ditt bilmärke:");
+            car.märke = Console.ReadLine();
+            Console.WriteLine("Skriv in din bilmodell:");
+            car.modell = Console.ReadLine();
+            Console.WriteLine("Skriv in bilens årsmodell:");
+            car.årsmodell = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in bilens färg:");
+            car.färg = Console.ReadLine();
+            Console.WriteLine("Skriv in bilens miltal:");
+            car.mil = int.Parse(Console.ReadLine());
+        }
+        public void metod2()
+        {
+            Console.WriteLine(car.märke);
+            Console.WriteLine(car.modell);
+            Console.WriteLine(car.årsmodell);
+            Console.WriteLine(car.färg);
+            Console.WriteLine(car.mil);
+        }
+    }
+
+}
---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp34
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bil car = new bil();
+            car.metod1();
+            car.metod2();
+
+        }
+    }
+}
